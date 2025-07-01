6aac1bf55e10c08932c742c66bd498cc9d82b568
RokuJS <shiuxcode@gmail.com>
2025-06-30 20:13:11 -0500

    Add prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   adapter: cloudflare(),
-  site: 'https://dignidadlaboral.org',
-  integrations: [sitemap()]
+  site: "https://dignidadlaboral.org",
+  integrations: [sitemap()],
 });
